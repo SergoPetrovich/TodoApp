da3d5992e063a3a662ce230534f2ef12d3f39701
SP <tk34moodle@tk34.mskobr.ru>
2022-03-04 16:09:34 +0300

    Работает.!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using TodoApp.Models;
+using TodoApp.Services;
 
 namespace TodoApp
 {
@@ -22,7 +11,9 @@ namespace TodoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
         private BindingList<TodoModel> _todoDataList;
+        private FileIOService _fileIOService;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,11 +21,18 @@ namespace TodoApp
                 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            _todoDataList = new BindingList<TodoModel>()
+            _fileIOService = new FileIOService(PATH);
+              try
             {
-                new TodoModel(){Text="test"},
-                new TodoModel(){Text="Test1"}
-            };
+                _todoDataList = _fileIOService.loadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+            
+
             dgTodoList.ItemsSource = _todoDataList;
             _todoDataList.ListChanged += _todoDataList_ListChanged;
 
@@ -42,23 +40,19 @@ namespace TodoApp
 
         private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
         {
-            switch (e.ListChangedType)
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
             {
-                case ListChangedType.Reset:
-                    break;
-                case ListChangedType.ItemAdded:
-                    break;
-                case ListChangedType.ItemDeleted:
-                    break;
-                case ListChangedType.ItemChanged:
-                    break;
-                case ListChangedType.PropertyDescriptorAdded:
-                    break;
-                case ListChangedType.PropertyDescriptorDeleted:
-                    break;
-                case ListChangedType.PropertyDescriptorChanged:
-                    break;
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    this.Close();
+                }
             }
+          
         }
     }
 }
--- Models/TodoModel.cs
+++ Models/TodoModel.cs
@@ -20,25 +20,36 @@ namespace TodoApp.Models
         public bool IsDone
         { 
             get { return _isDone; }
-            set { _isDone = value; } 
+            set { 
+                if(_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+                } 
         }
 
         
         public string Text
         {
             get { return _text; }
-            set { _text = value; }
+            set {
+                if (_text == value)
+                    return ;
+                _text = value;
+                OnPropertyChanged("Text");
+                }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged(string propertyName = "")
         {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
 
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            //if (PropertyChanged != null)
+            //{
+            //    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            //}
         }
     }
 }
